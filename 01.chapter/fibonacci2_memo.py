5f3b00372869cf2bfc40026a936314cb70093977
Kayo.io <kayorenatocontato@gmail.com>
2023-01-06 20:59:00 -0300

    add: Fibonacci Sequence

---
+++ 01.chapter/fibonacci2_memo.py
@@ -0,0 +1,13 @@
+from typing import Dict
+memo: Dict[int, int] = {0: 0, 1: 1} # base cases
+
+def fibonacci(n: int) -> int:
+    if n not in memo:
+        memo[n] = fibonacci(n-1) + fibonacci(n-2) # memorization
+        return memo[n]
+    return memo[n]
+
+if __name__ == "__main__":
+    print(fibonacci(5))
+    print(fibonacci(50))
+    print(memo)
