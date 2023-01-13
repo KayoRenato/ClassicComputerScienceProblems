c203fb57456b43d210c000a40532d15d0cf0fc24
Kayo.io <kayorenatocontato@gmail.com>
2023-01-13 17:32:59 -0300

    chore: add Linear Search

--- 02.searchProblems/dna_search.py
+++ 02.searchProblems/dna_search.py
@@ -6,7 +6,7 @@ Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
 Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]
 Gene = List[Codon]
 
-gene_str: str = "ACGTACGTACGTA"
+gene_str: str = "ACGTACGGTACGTA"
 
 
 def string_to_gene(s: str) -> Gene:
@@ -21,6 +21,23 @@ def string_to_gene(s: str) -> Gene:
         gene.append(codon)
     return gene
 
+
 my_gene: Gene = string_to_gene(gene_str)
 
 print(f'My Gene: {my_gene}')
+
+
+# Linear search
+def linear_contains(gene: Gene, key_codon: Codon) -> bool:
+    for codon in gene:
+        if codon == key_codon:
+            return True
+    return False
+
+acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
+gat: Codon = (Nucleotide.G, Nucleotide.T, Nucleotide.T)
+gta: Codon = (Nucleotide.G, Nucleotide.T, Nucleotide.A)
+
+print(linear_contains(my_gene, acg))
+print(linear_contains(my_gene, gat))
+print(linear_contains(my_gene, gta))
